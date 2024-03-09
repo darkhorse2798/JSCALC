1d5820a4179dcf7fb76c9ab0d777cc1fddb93b35
darkhorse2798 <nshaw204@gmail.com>
2024-03-09 14:06:05 +0530

    this is my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const clocks=document.getElementById('clock');
+
+// console.log(date.toLocaleString());
+setInterval(() => {
+    let date = new Date();
+    clocks.innerHTML=date.toLocaleTimeString();
+}, 1000);
